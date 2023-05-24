73fb5f8f2493a3576d7a3e981334d075c5013e43
DannyGGX <danielglanz111@gmail.com>
2023-05-24 11:06:30 +0200

    Added Player Movement
    Uses Character Controller
    Can't Jump

--- endless runner/Assets/_Scripts/Player/PlayerMovement.cs
+++ endless runner/Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,18 +4,81 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] Rigidbody rigidbody;
-    [SerializeField] private float forwardSpeed = 5;
-    [SerializeField] private float strafeSpeed = 2;
+    [SerializeField] private CharacterController characterController;
+    [Space]
+    [SerializeField] private float initialForwardSpeed;
+    [SerializeField] private float increaseForwardSpeed;
+    private float forwardSpeed;
+    private float forwardMovement;
+    [SerializeField] private float initialStrafeSpeed;
+    [SerializeField] private float increaseStrafeSpeed;
+    private float strafeSpeed;
+    private float strafeMovement;
+    [Space]
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float initialGravity;
+    [SerializeField] private float increaseGravityAmount;
+    private float gravity;
+    private float verticalMovement; // velocity
+    private Vector3 move;
+    private bool isGrounded = false;
+    [Space]
+    [SerializeField] private Transform groundCheckPositionForward;
+    [SerializeField] private Transform groundCheckPositionBackward;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckDistance;
+    [Space]
+    [SerializeField] private Transform leftBoundary;
+    [SerializeField] private Transform rightBoundary;
 
 
-    void Awake()
+    private void Awake()
     {
+        forwardSpeed = initialForwardSpeed;
+        strafeSpeed = initialStrafeSpeed;
+        gravity = initialGravity;
         
     }
 
     void Update()
     {
-        
+        strafeMovement = Input.GetAxis("Horizontal") * strafeSpeed;
+        forwardMovement = forwardSpeed;
+        verticalMovement += gravity * Time.deltaTime;
+
+        isGrounded = GroundCheck();
+        if(isGrounded && verticalMovement < 0)
+        {
+            verticalMovement = 1;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            verticalMovement = jumpForce;
+        }
+
+        move = new Vector3(strafeMovement, verticalMovement, forwardMovement);
+        characterController.Move(move * Time.deltaTime);
+    }
+
+    private bool GroundCheck()
+    {
+        if (Physics.Raycast(groundCheckPositionForward.position, Vector3.down, groundCheckDistance, whatIsGround))
+        {
+            return true;
+        }
+        if (Physics.Raycast(groundCheckPositionBackward.position, Vector3.down, groundCheckDistance, whatIsGround))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void IncreaseSpeed()
+    {
+        strafeSpeed += increaseStrafeSpeed;
+        forwardSpeed += increaseForwardSpeed;
+        gravity += increaseGravityAmount;
     }
 }
